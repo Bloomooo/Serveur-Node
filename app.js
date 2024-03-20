93b49d9e0e71f6af01b18bf856d4a02c905123b8
Yanis <mechtayanis20@gmail.com>
2024-03-20 13:13:55 +0100

    ajout du serveur + des connexions

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
-
 const app = express();
+const http = require("http");
+const socketIo = require("socket.io");
+const server = http.createServer(app);
+const PORT = process.env.PORT || 4001;
+const io = socketIo(server);
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Server is running");
 });
 
-app.listen(3000, () => {
-  console.log("====================================");
-  console.log("Server is running on port 3000");
-  console.log("====================================");
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 10000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
